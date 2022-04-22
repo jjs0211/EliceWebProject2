389b41289bd6a882de80da19805a3b7a2f744ba2
HyeJuSeon <hyejooseon@gmail.com>
2022-04-22 15:34:54 +0900

    [DIR]: swagger 세팅

--- back/src/app.js
+++ back/src/app.js
@@ -1,16 +1,41 @@
-import cors from 'cors';
-import express from 'express';
+import cors from "cors";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app = express();
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "DA API",
+      version: "1.0.0",
+      description: "Elice 데이터분석 팀프로젝트: 팀18 우아한 남매들",
+    },
+    servers: [
+      {
+        url: "http://localhost:" + process.env.SERVER_PORT,
+      },
+    ],
+  },
+  apis: [],
+};
+const specs = swaggerJsDoc(options);
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
 // 기본 페이지
-app.get('/', (req, res) => {
-  res.send('안녕하세요, 레이서 프로젝트 API 입니다.');
+app.get("/", (req, res) => {
+  res.send("안녕하세요, 레이서 프로젝트 API 입니다.");
 });
 
 export { app };
