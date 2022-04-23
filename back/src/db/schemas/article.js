4549521875907a628506e12d4e7485cbd7600aa8
jjs0211 <jjs_0211_@naver.com>
2022-04-24 00:39:51 +0900

    [DIR]: Article Schema 파일 생성

---
+++ back/src/db/schemas/article.js
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+
+const articleSchema = new Schema(
+    {
+    // article의 고유 id
+    id: {
+        type: String,
+        required: true,
+    },
+    // 게시글 작성자의 고유 id
+    userId: {
+      type: String,
+      required: true,
+    },
+    // 게시글 작성자
+    author: {
+        type: String,
+        required: true,
+      },
+
+    // 게시글 제목
+    title: {
+      type: String,
+      required: true,
+    },
+    // 게시글 내용
+    content: {
+      type: String,
+      required: true,
+    },
+    // 게시글 좋아요 수
+    likeCount: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+    // 게시글 조회수
+    visited: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+  },
+  {
+      // createdAt, updatedAt 자동 생성
+    timestamps: true,
+  }
+);
+
+const ArticleModel = model("Article", articleSchema);
+
+export { ArticleModel };
