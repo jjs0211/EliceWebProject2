70669976f06a80e178a4cb9bcfa51cbddc2071f7
jjs0211 <jjs_0211_@naver.com>
2022-04-26 14:22:05 +0900

    [COMMENT]: Article Model 주석 추가

--- back/src/db/models/Article.js
+++ back/src/db/models/Article.js
@@ -1,25 +1,25 @@
 import { ArticleModel } from "../schemas/article";
 
 class Article {
-    // 입력 받은 수상 정보 생성
+    // 입력 받은 게시글 정보 생성
     static async create({ newArticle }) {
       const createdNewArticle = await ArticleModel.create(newArticle);
       return createdNewArticle;
     }
     
-    // 수상이력의 고유한 id로 수상이력 검색(수정 할 때 사용)
+    // 게시글의 고유한 id로 게시글 검색(수정 할 때 사용)
     static async findById({ articleId }) {
       const article = await ArticleModel.findOne({ id: articleId });
       return article;
     }
 
-    // 입력받은 userId의 모든 수상이력 검색
+    // 입력받은 userId의 모든 게시글 검색
     static async findByUserId({ userId }) {
       const articles = await ArticleModel.find({ userId });
       return articles;
     }
     
-    // 입력받은 값들로 수상이력 업데이트(수정)
+    // 입력받은 값들로 게시글 업데이트(수정)
     static async update({ articleId, fieldToUpdate, newValue }) {
       const filter = { id: articleId };
       const update = { [fieldToUpdate]: newValue };
