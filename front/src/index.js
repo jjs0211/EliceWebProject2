e457a20a4bd658b2758a48c63ef041031e2bf1a2
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-04-28 02:33:28 +0900

    [REFACTOR] : App.js 라우팅 추가로 index.js 코드 수정

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-datepicker/dist/react-datepicker.css";
-import Main from './components/main/Main'
-import Login from './components/login/Login'
+import React from "react";
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import Main from './components/main/Main';
+import Login from './components/login/Login';
 import Register from "./components/register/Register";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Register/>
+    <App/>
   </React.StrictMode>,
   document.getElementById("root")
 );
