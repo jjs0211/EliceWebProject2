4cde1e246f2fcd95ec392481ab706023a713d305
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-04-28 11:45:50 +0900

    [REFACTOR] index.js code refactoring

--- front/src/index.js
+++ front/src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
-import Main from './components/main/Main';
-import Login from './components/login/Login';
-import Register from "./components/register/Register";
-import Blog from "./components/blog/Blog"
+
 
 ReactDOM.render(
   <React.StrictMode>
