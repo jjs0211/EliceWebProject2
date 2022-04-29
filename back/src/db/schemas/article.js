f77ca328a911684a60ad8c5c05cca57765b60e21
jjs0211 <jjs_0211_@naver.com>
2022-04-29 23:54:27 +0900

    [FEAT]: posting mvp CRUD, 조회수 완료

--- back/src/db/schemas/article.js
+++ back/src/db/schemas/article.js
@@ -3,10 +3,10 @@ import { Schema, model } from "mongoose";
 const articleSchema = new Schema(
     {
     // 게시글 작성자
-    author: {
+    loginId: {
         type: String,
         required: true,
-      },
+    },
     // 게시글 제목
     title: {
       type: String,
@@ -17,33 +17,28 @@ const articleSchema = new Schema(
       type: String,
       required: true,
     },
-    graphFilePath: {
+    // 게시글의 고유 id
+    id: {
       type: String,
       required: true,
     },
-
-    // // article의 고유 id
-    // id: {
-    //   type: String,
-    //   required: true,
-    // },
-    // // 게시글 작성자의 고유 id
-    // userId: {
-    //   type: String,
-    //   required: true,
-    // },
-    // // 게시글 좋아요 수
-    // likeCount: {
-    //     type: Number,
-    //     required: true,
-    //     default: 0,
-    //   },
-    // // 게시글 조회수
-    // visited: {
-    //     type: Number,
-    //     required: true,
-    //     default: 0,
-    // },
+    // 게시글 작성자의 고유 id
+    userId: {
+      type: String,
+      required: true,
+    },
+    // 게시글 좋아요 수
+    likeCount: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+    // 게시글 조회수
+    visited: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
   },
   {
       // createdAt, updatedAt 자동 생성
