e113f79cfc2be98adee2e8f1b69099ae6f361520
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-05-02 09:21:23 +0900

    [FEAT] Graph MVP Page design draft
    - 그래프 페이지 디자인 초안

---
+++ front/src/components/graph/Data/Data.js
@@ -0,0 +1,29 @@
+// Sidebar imports
+import {
+  UilClipboardAlt,
+  UilChart,
+} from "@iconscout/react-unicons";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilClipboardAlt,
+    heading: "Guide",
+  },
+  {
+    icon: UilChart,
+    heading: "Pie chart",
+  },
+  {
+    icon: UilChart,
+    heading: "Bar chart",
+  },
+  {
+    icon: UilChart,
+    heading: 'Radder chart'
+  },
+  {
+    icon: UilChart,
+    heading: 'Scatter chart'
+  },
+];
