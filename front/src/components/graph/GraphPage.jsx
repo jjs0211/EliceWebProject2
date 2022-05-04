d9486c95776163aa49f03c0729c5035185fc948b
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-05-04 13:21:09 +0900

    [FEAT] graphpage layout draft
    - Header 이미지 추가
    - Footer 이미지 추가
    - 대쉬보드 사이드 추가

--- front/src/components/graph/GraphPage.jsx
+++ front/src/components/graph/GraphPage.jsx
@@ -4,23 +4,63 @@ import '../../css/graph/GraphPage.css'
 import MainDash from './MainDash/MainDash'
 import LogoPart from './Header/LogoPart'
 import NavPart from './Header/NavPart';
+import GraphFooter from './Footer/GraphFooter';
+import GraphHeader from './Header/GraphHeader';
 
 
 const GraphPage = () => {
   return (
     <div>
-      <div className="Header">
+      <div className="graphHeaderContainer">
+        {/* <div className="graphHeaderImg">
+        </div> */}
+        <GraphHeader></GraphHeader>
+      </div>
+
+      {/* <div className="Header">
         <div className="HeaderGlass">
           <LogoPart></LogoPart>
+          <div>
+            <div id = "JustBlankBlock"></div>
+          </div>
+          <div>
+            <div>TEAMPAGE Button</div>
+          </div>
+        </div>
+      </div>
+      <div className="GraphPageNav">
+        <div className="GraphPageNavGlass">
           <NavPart></NavPart>
         </div>
       </div>
+
+      <div className="DashboardSelect">
+        <div>
+          <div>Guide</div>
+          <div>Donut</div>
+          <div>Bar</div>
+          <div>Radar</div>
+          <div>Bubble</div>
+        </div>
+      </div> */}
+
       <div className='GraphPage'>
         <div className="GraphPageGlass">
           <Sidebar></Sidebar>
+          <Sidebar></Sidebar>
           <MainDash></MainDash>
         </div>
-      </div>      
+      </div>
+
+      {/* <div className='GraphPageFooter'>
+        <div className='GraphPageFooterImg'>
+          <GraphFooter></GraphFooter>
+        </div>
+      </div> */}
+      <div className="GraphPageFooter">
+        <div className="GraphPageFooterImg">
+        </div>
+      </div>
     </div>
 
   )
