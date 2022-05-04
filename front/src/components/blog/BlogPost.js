063039c5f693b0f52a2d33f041467808f8507a03
Nojongchan <aprilgarden34@gmail.com>
2022-05-04 15:51:33 +0900

    글작성 페이지 handleUpload 함수로 백으로  전부 보낼수 있게 수정

--- front/src/components/blog/BlogPost.js
+++ front/src/components/blog/BlogPost.js
@@ -19,14 +19,6 @@ function BlogPost() {
   const [userId, setUserId] = useState('')
 
 
-  // user/current로 현재 유저 정보를 백에서 불러옴. 그 뒤 loginid를 받아온다.
-
-    async function userInfo() {
-      const res = await Api.get('user/current')
-      const userInfo = res.data.loginId
-      console.log(userInfo) 
-      setUserId(userInfo)
-  }
 
   // Quill에 작성한 글자 정보. body라는 스테이트에 저장.
 
@@ -64,27 +56,23 @@ function BlogPost() {
 
   // 이미지 업로드 함수.. 버튼 클릭시 입력된 이미지를 폼데이터 형식으로 전환, // APi로 백으로 전송, // 관련 Api는 이름이랑 주소를 몰라 아직 작성 안했습니다. multer는 가칭입니다. // 백에서 맞춰고 알려주세요.
   
-  // const handleUpload = async (e) => {
-  //   const formData = new FormData()
-  //   formData.append("image", image)
-  //   const res = await Api.imagePost("article/uploadFile", formData)
-  //   console.log(res)  
-  // }
+  const handleUpload = async () => {
+    
+    const formData = new FormData()
+    formData.append("image", image)
+    const filePath = await Api.imagePost("article/uploadFile", formData)
+     
+    const data = {
+      nickName: nickname,
+      title: title,
+      content: body,
+      filePath: filePath,
+    }
 
-  // const handleUpload = async (e) => {
-  //   const data = {
-  //     "nickName": nickname,
-  //     "title": title,
-  //     "content": body,
-  //     "filePath": title,
-  //   }
-  //   const res = await Api.post("article/create", data)
-  //   console.log(res)
-  // }
+    const res = await Api.post("article/create", data)
+    console.log(res)
+  }
  
-
-
-
 
   return (
   <div className="QuillContainer">
@@ -92,7 +80,7 @@ function BlogPost() {
     <input className="QuillTitle" type="text" placeHolder="제목을 입력해주세요" onChange={handleTitle}></input>
     <input className="QuillNickname" type='text' placeholder='닉네임' onChange={handleNickname} ></input>
     <button className="summitTextButton" 
-      // onClick={handleUpload}
+      onClick={handleUpload}
       disabled={!(imageComplete && titleComplete && nicknameComplete)}>글 입력</button>
     </div>
     
