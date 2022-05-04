4bc07681c411f82ec1e60ba1dadd6879dbd1d233
Nojongchan <aprilgarden34@gmail.com>
2022-05-04 17:49:54 +0900

    블로그 페이지의 작은 기사를 누를시 상세 기사로 가도록 navigation기능 준비(완성x)

--- front/src/components/blog/BlogArticlePreview.js
+++ front/src/components/blog/BlogArticlePreview.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../../css/blog/BlogArticlePreview.css'
+import { useNavigate, useParams } from "react-router-dom";
 
 function BlogArticlePreview(props) {
+  const navigate = useNavigate()
 
   return (
 
-    <div className="PreviewContainer">
+    <div className="PreviewContainer" onClick={() => navigate('/main')}>
       <div className="PreviewimageBox">
         <img src={props.src} alt='사진'></img>
       </div>
