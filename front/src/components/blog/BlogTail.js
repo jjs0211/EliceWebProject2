31dbe11ba25a007c72d53eca7f5fe8eca87a6f49
Nojongchan <aprilgarden34@gmail.com>
2022-05-06 19:19:33 +0900

    블로그 화면 디자인 작은 수정

--- front/src/components/blog/BlogTail.js
+++ front/src/components/blog/BlogTail.js
@@ -24,7 +24,7 @@ const [articleList, setArticleList] = useState('')
 
 
   return (
-    <div className="blogTailContainer">
+    <>
       <div className="blogTailTitleBox">
         <p className="blogTailTitle">조회수 Top5</p>
       </div>
@@ -33,8 +33,8 @@ const [articleList, setArticleList] = useState('')
         <BlogSlider articleList={articleList} />
         </div>
       </div>
-    </div>
-  )
+    </>
+  )   
 
 }
 
