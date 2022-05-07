65992922777b67d94facd373ab050ff8efc94039
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-05-07 18:52:36 +0900

    [FEAT] Raddar 그래프 컴포넌트에 state로 데이터 가져오기

--- front/src/components/graph/Apex/Radar.jsx
+++ front/src/components/graph/Apex/Radar.jsx
@@ -15,14 +15,16 @@ import axios from "axios";
 // })
 
 
+const Radar = (state) =>{
 
+  // constructor(props) 
+  
+  //   super(props);
+    console.log('레이다 그래프!!!11')
+    console.log(state.data)
+        console.log('레이다 그래프!!!22')
 
-class Radar extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
+    state = {
       options: {
       xaxis: {
       categories: ['Carbs', 'Fiber', 'Fat', 'Protein', 'SatFat'],
@@ -32,9 +34,9 @@ class Radar extends Component {
           colors: ["#a8a8a8"],
           fontSize: "20px",
           fontFamily: 'Arial'
-  }
-}
-}
+          }
+        }
+        }
 
       },
       series: [
@@ -50,17 +52,66 @@ class Radar extends Component {
   ],
       labels: ['Carbs', 'Fiber', 'Fat', 'Protein', 'SatFat'],
 
-    }
+    
   }
 
-  render() {
-
+  
     return (
       <div className="donut">
-        <Chart options={this.state.options} series={this.state.series} type="radar" width="700" />
+        <Chart options={state.options} series={state.series} type="radar" width="700" />
       </div>
     );
-  }
+  
 }
+
+
+// class Radar extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     console.log('레이다 그래프!!!11')
+//     // console.log(porps.data)
+//         console.log('레이다 그래프!!!22')
+
+//     this.state = {
+//       options: {
+//       xaxis: {
+//       categories: ['Carbs', 'Fiber', 'Fat', 'Protein', 'SatFat'],
+//       labels: {
+//         show: true,
+//         style: {
+//           colors: ["#a8a8a8"],
+//           fontSize: "20px",
+//           fontFamily: 'Arial'
+//           }
+//         }
+//         }
+
+//       },
+//       series: [
+//     {
+//       name: "IceCream",
+//       data: [45, 52, 38, 24, 33]
+//     },
+//     {
+//       name: "Pie",
+//       data: [26, 21, 20, 6, 8]
+//         },
+    
+//   ],
+//       labels: ['Carbs', 'Fiber', 'Fat', 'Protein', 'SatFat'],
+
+//     }
+//   }
+
+//   render() {
+
+//     return (
+//       <div className="donut">
+//         <Chart options={this.state.options} series={this.state.series} type="radar" width="700" />
+//       </div>
+//     );
+//   }
+// }
 
 export default Radar;
